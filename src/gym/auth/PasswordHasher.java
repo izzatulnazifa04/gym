d38017de323a29e9f6d49b42d4a0a156c1fd229c
izzati1609 <s251594717@studentmail.unimap.edu.my>
2026-09-08 20:35:13 +0800

    Add import statements for security and hashing in PasswordHasher class

--- src/gym/auth/PasswordHasher.java
+++ src/gym/auth/PasswordHasher.java
@@ -1,1 +1,8 @@
 package gym.auth;
+
+import java.security.GeneralSecurityException; //handle security errors
+import java.security.MessageDigest; //nk create hash
+import java.security.SecureRandom; //generate random salt
+import java.util.Base64; //decode/decode
+import java.crypto.SecretKeyFactory; //generate secret code
+import java.crypto.spec.PBEKeySpec; //utk simpan pass n setting tuk encrypt
