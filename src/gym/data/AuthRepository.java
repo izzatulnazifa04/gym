6141ac942d904dccb23ac211207e30d7c2457330
neshunimap <s251594700@studentmail.unimap.edu.my>
2026-09-11 16:00:27 +0800

    Add initial implementation of AuthRepository class

---
+++ src/gym/data/AuthRepository.java
@@ -0,0 +1,12 @@
+package gym.data;
+
+import gym.auth.PasswordHasher;
+import gym.model.UserAccount;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
