111c4f6f6fd6d2391f7a742c40b8095fd4829fe4
neshunimap <s251594700@studentmail.unimap.edu.my>
2026-09-12 17:54:38 +0800

    Add fromDatabase method to MembershipStatus enum for handling null values

--- src/gym/model/MembershipStatus.java
+++ src/gym/model/MembershipStatus.java
@@ -4,3 +4,8 @@ public enum MembershipStatus {
     ACTIVE,
     EXPIRED,
     SUSPENDED;
+
+    public static MembershipStatus fromDatabase(String value) {
+        if (value == null) {
+            return ACTIVE;
+        }
