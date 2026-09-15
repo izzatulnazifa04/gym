d16837aae5b38ab3c708d63da1faa78ffd4d1743
izzati1609 <s251594717@studentmail.unimap.edu.my>
2026-09-15 11:41:56 +0800

    add constructors to YearlyMembership class for initializing discountPercent

--- src/gym/YearlyMembership.java
+++ src/gym/YearlyMembership.java
@@ -8,5 +8,15 @@ public class YearlyMembership extends Membership {
         super();
         discountPercent = 0.0;
     }
+
+    public YearlyMembership(String memberName, String icNumber, String startDate, double rate, double discountPercent) {
+        super(memberName, icNumber, startDate, rate);
+        this.discountPercent = discountPercent;
+    }
+
+    public YearlyMembership(int memberID, String memberName, String icNumber, String startDate, double rate, double discountPercent) {
+        super(memberID, memberName, icNumber, startDate, rate);
+        this.discountPercent = discountPercent;
+    }
     
 }
