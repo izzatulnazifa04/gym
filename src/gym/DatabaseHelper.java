23549c901730c0c46734a2ec523373605b5a93d8
izzati1609 <s251594717@studentmail.unimap.edu.my>
2026-09-16 07:27:27 +0800

    edit

--- src/gym/DatabaseHelper.java
+++ src/gym/DatabaseHelper.java
@@ -19,7 +19,7 @@ public class DatabaseHelper {
 
     public static void initializeDatabase() {
         String sql = "CREATE TABLE IF NOT EXISTS memberships ("
-                + "member_id INTEGER PRIMARY KEY, "
+                + "member_id INTEGER PRIMARY KEY AUTOINCREMENT, "
                 + "member_name TEXT NOT NULL, "
                 + "ic_number TEXT NOT NULL UNIQUE, "
                 + "membership_type TEXT NOT NULL, "
